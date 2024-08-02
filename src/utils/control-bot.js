20b109d77d8c7b0538796cb92372c950979f1c8d
AngelDeLeon96 <ajdla96@gmail.com>
2024-08-02 15:58:09 -0500

    upgraded

--- src/utils/control-bot.js
+++ src/utils/control-bot.js
@@ -1,6 +1,16 @@
 // state.js
-const controlBot = {
-    status: true,
+const controlBot = (phone, bot, remainingTime, timers, TIMER_BOT) => {
+    if (!bot.dynamicBlacklist.checkIf(phone)) {
+        // Si el bot no está desactivado para este usuario
+        const elapsedTime = TIMER_BOT - (Date.now() - timers[phone]);
+        if (timers[phone]) {
+            remainingTime[phone] = elapsedTime;
+            // Borra el temporizador actual
+            clearTimeout(timers[phone]);
+            timers[phone] = null;
+        }
+        console.log(`Tiempo restante para ${phone}: ${remainingTime[phone]} ms`);
+    }
 };
 
 export default controlBot;
