d8f5a25492fcc88bf37175f005307e787a48e31b
AngelDeLeon96 <ajdla96@gmail.com>
2024-08-02 15:57:05 -0500

    log quitado

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-console.log(__dirname)
+//console.log(__dirname)
 i18n.configure({
     locales: ['es'],
     defaultLocale: 'es',
