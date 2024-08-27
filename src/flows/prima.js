192d2d767cf43db218e972912312f5e6a3482000
AngelDeLeon96 <ajdla96@gmail.com>
2024-08-27 15:48:57 -0500

    upgraded flow prima menu and other fixes

--- src/flows/prima.js
+++ src/flows/prima.js
@@ -5,7 +5,7 @@ import { reset, start, stop } from '../utils/timer.js';
 import { showMSG } from '../i18n/i18n.js';
 import { freeFlow } from './agents.js';
 import Queue from 'queue-promise';
-import { saveMediaWB, getMimeWB } from '../utils/utils.js';
+import { saveMediaWB, getMimeWB, extractMimeWb } from '../utils/utils.js';
 
 
 const queue = new Queue({
@@ -15,12 +15,11 @@ const queue = new Queue({
 
 const prima_menu = addKeyword(EVENTS.ACTION)
     .addAction(async (ctx, { gotoFlow }) => start(ctx, gotoFlow))
-    .addAnswer([showMSG('menu'), '1. Solicitud por primera vez.', '2. Adjuntar formulario de solictud y copia de cédula.', '3. (Opcional)Adjuntar de certificaciones de continuidad de otras instituciones.', '4. Consulta del trámite en curso (hablar con un agente).', '5. Salir'], { capture: true }, async (ctx, { state }) => {
+    .addAnswer([showMSG('menu'), showMSG('prima_opcion_1'), showMSG('prima_opcion_2'), showMSG('prima_opcion_3'), showMSG('prima_opcion_4'), `5. ${showMSG('exit')}`], { capture: true }, async (ctx, { state }) => {
         reset(ctx)
         await state.update({ 'prima_menu_opc': ctx.body })
     })
     .addAction(async (ctx, { state, gotoFlow, endFlow, fallBack, globalState }) => {
-
         sendMessageChatwood(`${showMSG('selected')} ${state.get('prima_menu_opc')}`, 'incoming', globalState.get('conversation_id'));
         switch (parseInt(state.get('prima_menu_opc'))) {
             case 1:
@@ -45,48 +44,43 @@ const prima_menu = addKeyword(EVENTS.ACTION)
     })
 
 const primera_vez = addKeyword(EVENTS.ACTION)
-    .addAction(async (ctx, { gotoFlow }) => start(ctx, gotoFlow))
+    .addAnswer(`${showMSG('selected')} ${showMSG('prima_opcion_1')}`)
     .addAnswer(`${showMSG('llenar_form')}`, { media: path.join(process.cwd(), 'public/files', 'FGE-solicitud y declaracion CO.pdf') })
-    .addAnswer(`${showMSG('subir_pdf')}`, { capture: true }, async (ctx, { fallBack, globalState }) => {
-        reset(ctx)
-        let typeMSG = getMimeWB(ctx.message)
-        if (typeMSG === "senderKeyDistributionMessage") {
-            return fallBack(showMSG('no_permitida'));
-        }
-        else {
-            let [msg, attachment] = await saveMediaWB(ctx)
-            sendMessageChatwood(msg, 'incoming', globalState.get('conversation_id'), attachment);
-        }
-    })
-    .addAnswer(`${showMSG('formulario_captado')}`)
-    .addAction(async (ctx, { gotoFlow }) => {
-        stop(ctx)
+    .addAnswer(`${showMSG('subir_pdf')} `)
+    .addAction({ delay: 700 }, async (_, { gotoFlow }) => {
         //return endFlow(`${showMSG('formulario_captado')}`)
         return gotoFlow(prima_menu)
     })
 
+//flow para attach los docs de form, cedula.
 const attach_forms = addKeyword(EVENTS.ACTION)
     .addAction(async (ctx, { gotoFlow }) => start(ctx, gotoFlow))
-    .addAnswer(`${showMSG('adjuntar_form')}`, async (ctx, { fallBack, globalState }) => {
+    .addAnswer(`${showMSG('selected')} ${showMSG('prima_opcion_2')}`)
+    .addAnswer(`${showMSG('adjuntar_form')}`, { capture: true }, async (ctx, { fallBack, globalState }) => {
         reset(ctx)
-        let typeMSG = getMimeWB(ctx.message)
-        if (typeMSG === "senderKeyDistributionMessage") {
-            return fallBack(showMSG('no_permitida'));
-        }
-        else {
+        //console.log('body: ', JSON.stringify(ctx))
+        let extractedMime = extractMimeWb(ctx)
+        let formats = ['pdf', 'docx']
+        if (formats.includes(extractedMime)) {
             let [msg, attachment] = await saveMediaWB(ctx)
             sendMessageChatwood(msg, 'incoming', globalState.get('conversation_id'), attachment);
+        }
+        else {
+            return fallBack(`${showMSG('no_permitida')} Solo se permiten los archivos en formato: ${formats}. ${showMSG('try_again')}`);
         }
     })
+    .addAnswer(showMSG('formulario_captado'))
     .addAnswer(`${showMSG('adjuntar_cedula')}`, { capture: true }, async (ctx, { fallBack, globalState }) => {
         reset(ctx)
-        let typeMSG = getMimeWB(ctx.message)
-        if (typeMSG === "senderKeyDistributionMessage") {
-            return fallBack(showMSG('no_permitida'));
-        }
-        else {
+        //console.log('body: ', JSON.stringify(ctx))
+        let extractedMime = extractMimeWb(ctx)
+        let formats = ['pdf', 'docx', 'png', 'jpg']
+        if (formats.includes(extractedMime)) {
             let [msg, attachment] = await saveMediaWB(ctx)
             sendMessageChatwood(msg, 'incoming', globalState.get('conversation_id'), attachment);
+        }
+        else {
+            return fallBack(`${showMSG('no_permitida')} Solo se permiten los archivos en formato: ${formats}. ${showMSG('try_again')}`);
         }
     })
     .addAnswer(`${showMSG('formulario_captado')}`)
@@ -97,17 +91,20 @@ const attach_forms = addKeyword(EVENTS.ACTION)
     })
 
 
-const attach_forms_continuidad = addKeyword(EVENTS.ACTION)
+const attach_forms_continuidad = addKeyword('attach2')
     .addAction(async (ctx, { gotoFlow }) => start(ctx, gotoFlow))
+    .addAnswer(`${showMSG('selected')} ${showMSG('prima_opcion_3')}`)
     .addAnswer(`${showMSG('adjuntar_continuidad')}`, { capture: true }, async (ctx, { globalState, fallBack }) => {
         reset(ctx)
-        let typeMSG = getMimeWB(ctx.message)
-        if (typeMSG === "senderKeyDistributionMessage") {
-            return fallBack(showMSG('no_permitida'));
+        let extractedMime = extractMimeWb(ctx)
+        //console.log('ext detected: ', extractedMime)
+        let formats = ['pdf', 'docx']
+        if (formats.includes(extractedMime)) {
+            let [msg, attachment] = await saveMediaWB(ctx)
+            await sendMessageChatwood(msg, 'incoming', globalState.get('conversation_id'), attachment);
         }
         else {
-            let [msg, attachment] = await saveMediaWB(ctx)
-            sendMessageChatwood(msg, 'incoming', globalState.get('conversation_id'), attachment);
+            return fallBack(`${showMSG('no_permitida')} Solo se permiten los archivos en formato: ${formats}. ${showMSG('try_again')}`);
         }
     })
     .addAnswer(`${showMSG('formulario_captado')}`)
