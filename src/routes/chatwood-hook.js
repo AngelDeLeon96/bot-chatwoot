f5b2bb4160b2cda726c5a349a79a7e1f6aff4a15
AngelDeLeon96 <ajdla96@gmail.com>
2024-09-05 15:17:29 -0500

    fixed fun idAssigned

--- src/routes/chatwood-hook.js
+++ src/routes/chatwood-hook.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const TIMER_BOT = process.env.TIMER_BOT ?? 600000
 import { timers } from '../utils/timer.js';
 import logger from '../utils/logger.js';
+import { break_flow } from '../utils/utils.js';
+import { sendMessageChatwood } from '../services/chatwood.js';
+
 // Enviar mensaje a usuario de WhatsApp
 //console.log(TIMER_BOT / 60000)
 const chatWoodHook = async (req, res) => {
@@ -14,17 +17,16 @@ const chatWoodHook = async (req, res) => {
         const body = req.body;
         //const mapperAttributes = body?.conversation?.meta?.assignee;
         const mapperAttributes = findMyData(body, 'assignee');
-
+        //console.log(JSON.stringify(body), '====', mapperAttributes);
         const attachments = body?.attachments;
         const phone = body.conversation?.meta?.sender?.phone_number.replace('+', '');
         const content = body.content;
         const status = body.status;
         const event = body.event;
-        const keywords = ['hasta luego', 'adios', 'resuelto'];
-        let partial = content ? keywords.includes(content.normalize('NFD').toLowerCase().replace(/[\u0300-\u036f]/g, "")) : false
+        let break_word = break_flow(content);
         let partial_status = (status === "resolved" && event === "conversation_updated")
 
-        if (partial || partial_status) {
+        if (break_word || partial_status) {
             let phone_check = bot.dynamicBlacklist.checkIf(phone)
             //console.log('capturando el texto clave o cambio de estado.', partial, partial_status, phone_check)
             if (phone_check) {
@@ -39,15 +41,16 @@ const chatWoodHook = async (req, res) => {
         /*La parte que se encarga de enviar un mensaje al whatsapp del cliente*/
         const checkIfMessage = body?.private == false && body?.event == "message_created" && body?.message_type === "outgoing" && body?.conversation?.channel.includes("Channel::Api")
         //console.log(`checkif`, checkIfMessage, '\n')
-        if (checkIfMessage) {
+        if (checkIfMessage && mapperAttributes !== null) {
             //console.log('mensaje enviado desde CRM', `MSG is: ${body?.content}`, checkIfMessage, Date.now())
             const content = body?.content ?? '';
             const file = attachments?.length ? attachments[0] : null;
             //console.log(mapperAttributes)
             if (body?.event === 'message_created' && Object.hasOwn(mapperAttributes, 'id')) {
-                const idAssigned = mapperAttributes.id ?? true
-                //const idAssigned = true
-                console.log('idAssigned: ', idAssigned)
+                const idAssigned = mapperAttributes.id ?? true;
+                console.log('idAssigned: ', idAssigned);
+                //const idAssigned = true;
+
                 if (idAssigned) {
                     //console.log(`${phone} blocked...`)
                     if (!bot.dynamicBlacklist.checkIf(phone) && !timers[phone]) {
@@ -60,7 +63,12 @@ const chatWoodHook = async (req, res) => {
                             //console.log(`bot reactivated...`);
                         }, TIMER_BOT);
                     }
+                } else {
+                    console.log('Agente no asignado...');
                 }
+            }
+            else {
+                console.log('Agente no asignado...');
             }
             //envia los docs al whatsapp
             if (file) {
@@ -76,6 +84,9 @@ const chatWoodHook = async (req, res) => {
             //res.send('ok');
             //console.log('=>OK')
             return;
+        }
+        else {
+            logger.info(`No se cumple con las condiciones para enviar mensaje... =>`)
         }
         //res.send(body)
         res.send('ok')
